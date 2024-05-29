10e20614429431c0dcf23c905bb4ff56a87bf836
Ibrahim Labib <ibrahimlabib4002@gmail.com>
2024-05-29 13:37:47 +0600

    nxt change

--- project3NUMBERGUESSINGGAME.py
+++ project3NUMBERGUESSINGGAME.py
@@ -1,11 +1,13 @@
 import random
-randomnums = random.randint(1,200)
-print(randomnums)
-usermsg = int(input("Enter a number: "))
-#well guessed one time :)
-if usermsg > randomnums:
-    print("Number is too Big")
-elif usermsg < randomnums:
-    print("Number is too small")
-else:
-    print("correct!")
+
+def guess(x):
+    random_num = random.randint(1,x)
+    guess = ""
+    while guess != random_num:
+        guess = int(input(f"Guess the number between 1 to {x} : "))
+        if guess < random_num:
+            print("Your number is too small")
+        elif guess > random_num:
+            print("Your number is too big")
+    print(f"Yay , you successfully guessed the number which is {guess}")
+guess(10)
