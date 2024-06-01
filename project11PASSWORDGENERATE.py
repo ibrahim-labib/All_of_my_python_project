e419200624b5d31849a45baecf73ac5400742b0d
Ibrahim Labib <ibrahimlabib4002@gmail.com>
2024-06-01 21:21:40 +0600

    password generator

--- project11PASSWORDGENERATE.py
+++ project11PASSWORDGENERATE.py
@@ -1,21 +1,20 @@
 import random
 
-def password_generator(num_password, password_lenght):
+def password_generator(num_password, password_length):
     passwords = []
     char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@&#"
     for i in range(num_password):
         password = ""
-        for i in range(password_lenght):
+        for j in range(password_length):
             password += random.choice(char)
         passwords.append(password)
     
     return passwords
 
-
-def valid_int(promt):
+def valid_int(prompt):
     while True:
         try:
-            value = int(input(promt))
+            value = int(input(prompt))
             if value < 0:
                 print("Please enter a positive integer value.")
             else:
@@ -23,20 +22,18 @@ def valid_int(promt):
         except ValueError:
             print("Please enter a valid integer.")
 
-
 def main_screen():
+    print("Welcome to the password generator.")
     while True:
-        print("Welcome to the password generator.")
-        num_password = valid_int("Number of password: ")
-        password_lenght = valid_int("Password lenght: ")
-        passwords = password_generator(num_password,password_lenght)
-
-        
+        num_password = valid_int("Number of passwords: ")
+        password_length = valid_int("Password length: ")
+        passwords = password_generator(num_password, password_length)
+    
         for password in passwords:
             print(password)
-
+        
         user_quit = input("Do you want to quit? [y/n]: ")
-        if user_quit.lower() == "y":
+        if user_quit == "y":
             break 
 
 if __name__ == "__main__":
